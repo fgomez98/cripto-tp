1348760bd5f81f6a260419a30bb236b26416af56
pedroremigiopingarilho <premigio@itba.edu.ar>
2020-06-10 20:13:28 -0300

    arch

---
+++ src/main/java/itba/edu/ar/api/LSBI.java
@@ -0,0 +1,13 @@
+package itba.edu.ar.api;
+
+public class LSBI implements LSB{
+    @Override
+    public byte[] encrypt(byte[] message, byte[] bmp) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] decrypt(byte[] message) {
+        return new byte[0];
+    }
+}
